fdb8f7aff7122d5d3f4edb3a16e8e1183685d774
Mircea Toma <mircea.toma@icesoft.com>
2023-08-16 19:58:49 +0300

    Start implementation.

--- src/memo/scheduler.clj
+++ src/memo/scheduler.clj
@@ -7,26 +7,41 @@
             [langohr.basic :as amqp#basic]
             [taoensso.timbre :refer [trace debug info warn error spy]]))
 
+(def queue-name "memo-internal")
+
 (defprotocol Scheduler
   (schedule [self dest cron message])
   (unschedule [self id])
   (schedules [self])
   (shutdown [self]))
 
-(deftype AmqpScheduler [url]
+(deftype AmqpScheduler [connection ch]
   Scheduler
   (schedule [self dest cron message]
-    (debug (str "dest: " dest " cron: " cron " message: " message)))
+    (debug (str "dest: " dest " cron: " cron " message: " message))
+    (amqp#basic/publish ch "" queue-name message {:content-type "text/plain"}))
   (unschedule [self id]
     (debug (str "id: " id)))
   (schedules [self]
     (debug "schedules"))
   (shutdown [self]
     (info "Stopping scheduler...")
+    (amqp#core/close connection)
     (info "Stopped scheduler.")))
+
+(defn message-handler
+  [ch {:keys [content-type delivery-tag] :as meta} ^bytes payload]
+  (debug
+    (str "Received a message: " (String. payload "UTF-8") ", delivery tag: " delivery-tag ", content type: " content-type)))
 
 (defn run [url]
   (info "Starting scheduler...")
-  (let [scheduler (AmqpScheduler. url)]
+  (debug "Using URL: " url)
+  (let [connection (amqp#core/connect {:uri url})
+        ch (amqp#channel/open connection)
+        scheduler (AmqpScheduler. connection ch)]
+    (amqp#queue/declare ch queue-name {:exclusive false :auto-delete true})
+    (amqp#consumer/subscribe ch queue-name message-handler {:auto-ack true})
+
     (info "Started scheduler.")
     scheduler))
