36378abe3388e67bf4d713a7f872aa8af41383e1
Mircea Toma <mircea.toma@icesoft.com>
2023-08-17 15:21:48 +0300

    Factor out queue/exchange setup. Mark messages, add TTL.

--- src/memo/scheduler.clj
+++ src/memo/scheduler.clj
@@ -7,7 +7,11 @@
             [langohr.basic :as amqp#basic]
             [taoensso.timbre :refer [trace debug info warn error spy]]))
 
-(def queue-name "memo-internal")
+(def queue-name "memo.internal")
+(defn- setup-queues [ch]
+  (amqp#queue/declare ch queue-name {:durable true
+                                     :exclusive false
+                                     :auto-delete false}))
 
 (defprotocol Scheduler
   (schedule [self dest cron message])
@@ -17,23 +21,33 @@
 
 (deftype AmqpScheduler [connection ch]
   Scheduler
+
   (schedule [self dest cron message]
     (debug (str "dest: " dest " cron: " cron " message: " message))
-    (amqp#basic/publish ch "" queue-name message {:content-type "text/plain"}))
+    (let [id (str (random-uuid))
+          attributes {:message-id id :content-type "text/plain" :persistent true :expiration "400"}]
+      (amqp#basic/publish ch "" queue-name message attributes)))
+
   (unschedule [self id]
     (debug (str "id: " id)))
+
   (schedules [self]
     (debug "schedules")
     [])
+
   (shutdown [self]
     (info "stopping scheduler...")
     (amqp#core/close connection)
     (info "stopped scheduler")))
 
-(defn message-handler
-  [ch {:keys [content-type delivery-tag] :as meta} ^bytes payload]
+(defn message-handler [ch meta ^bytes payload]
+  (spy meta)
   (debug
-    (str "received a message: " (String. payload "UTF-8") ", delivery tag: " delivery-tag ", content type: " content-type)))
+    (str "received a message: " (String. payload "UTF-8")))
+  (let [delivery-tag (:delivery-tag meta)]
+    (if (< delivery-tag 100)
+      (amqp#basic/reject ch delivery-tag true)
+      (amqp#basic/ack ch delivery-tag))))
 
 (defn run [url]
   (info "starting scheduler...")
@@ -41,8 +55,8 @@
   (let [connection (amqp#core/connect {:uri url})
         ch (amqp#channel/open connection)
         scheduler (AmqpScheduler. connection ch)]
-    (amqp#queue/declare ch queue-name {:exclusive false :auto-delete true})
-    (amqp#consumer/subscribe ch queue-name message-handler {:auto-ack true})
+    (setup-queues ch)
+    (amqp#consumer/subscribe ch queue-name message-handler {:auto-ack false})
 
     (info "started scheduler")
     scheduler))
